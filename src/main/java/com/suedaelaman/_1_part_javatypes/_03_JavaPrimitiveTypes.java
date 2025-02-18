eacbad9bd488882cca76410480496eb0a8e756eb
Sueda Elaman <suedaelaman@gmail.com>
2025-02-18 13:47:58 +0300

    Data variables names rules

---
+++ src/main/java/com/suedaelaman/_1_part_javatypes/_03_JavaPrimitiveTypes.java
@@ -0,0 +1,13 @@
+package com.suedaelaman._1_part_javatypes;
+
+public class _03_JavaPrimitiveTypes {
+
+    public static void main(String[] args) {
+        // Veri değişken isimlerini yazarken;
+        // tam sayılar (4)
+        byte b1=127;
+        // char
+        char c4= '\u4125';
+        System.out.println("char: "+c4);
+    }
+}
