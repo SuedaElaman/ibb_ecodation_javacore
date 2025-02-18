86c72fa3a5e1c9d580c9b873d891d08017c76b40
Sueda Elaman <suedaelaman@gmail.com>
2025-02-18 13:02:31 +0300

    class,psvm,sout

--- src/main/java/com/suedaelaman/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/suedaelaman/_1_part_javatypes/_01_JavaTypes.java
@@ -1,4 +1,10 @@
 package com.suedaelaman._1_part_javatypes;
 
 public class _01_JavaTypes {
-}
+    // psvm => tab
+    public static void main(String[] args) {
+        //çıktı
+        //sout => tab
+        System.out.println("Sueda Elaman");
+    } //end psvm
+} //end class
